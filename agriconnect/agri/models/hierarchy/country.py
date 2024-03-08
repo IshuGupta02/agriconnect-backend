fd9ac8da4a04d7a4df3440e8d695651c2bcc9f08
ishugupta <ishugupta0298@gmail.com>
2024-03-09 02:31:53 +0530

    Add crop, msp pair in Country

--- agriconnect/agri/models/hierarchy/country.py
+++ agriconnect/agri/models/hierarchy/country.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+class CropMSP(models.Model):
+    crop = models.ForeignKey(to='Crop', on_delete=models.CASCADE)
+    msp = models.FloatField()
+    country = models.ForeignKey(to = 'Country', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.crop} - MSP: {self.msp}"
+
 class Country(models.Model):
     name = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return self.name
