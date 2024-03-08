f23feb8e547a9ee048b487df26232f71d391b556
ishugupta <ishugupta0298@gmail.com>
2024-03-09 01:58:35 +0530

    Register models on admin site

--- agriconnect/agri/admin.py
+++ agriconnect/agri/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from agri.models import *
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(District)
+admin.site.register(Village)
