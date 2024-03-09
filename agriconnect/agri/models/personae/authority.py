e733a195f90c2acfe4a89c6dda6bb323fa1554f4
ishugupta <ishugupta0298@gmail.com>
2024-03-09 11:12:05 +0530

    Resolve migration errors

--- agriconnect/agri/models/personae/authority.py
+++ agriconnect/agri/models/personae/authority.py
@@ -1,6 +1,7 @@
 from django.db import models
+from .person import Person
 
-class Authority(models.Model):
+class Authority(Person):
     report_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subordinates')
     farms = models.ManyToManyField(to='Farmer', related_name='authorities')
 
--- agriconnect/agriconnect/settings.py
+++ agriconnect/agriconnect/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'agri',
 ]
 
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'agriconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
